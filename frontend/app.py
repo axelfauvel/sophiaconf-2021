945941da28123bc15d88b513df64de1af35926ee
Axel FAUVEL <axel.fauvel@gmail.com>
2021-04-25 20:17:13 +0000

    use redis dynamic data

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 import os
+from data import get_device_data
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-# APP_STATIC = os.path.join(APP_ROOT, "static")
 APP_TPL = os.path.join(APP_ROOT, "templates")
 
 app = Flask(__name__)
@@ -21,20 +21,7 @@ def bar_chart_js():
 
 @app.route("/line-chart.js")
 def line_chart_js():
-    data = [
-        {"time": 0, "device": "green", "value": 1.336},
-        {"time": 1, "device": "green", "value": 6.522},
-        {"time": 2, "device": "green", "value": 12.867},
-        {"time": 3, "device": "green", "value": 17.821},
-        {"time": 4, "device": "green", "value": 20.634},
-        {"time": 5, "device": "green", "value": 25.68},
-        {"time": 6, "device": "green", "value": 31.003},
-        {"time": 7, "device": "green", "value": 39.009},
-        {"time": 8, "device": "green", "value": 48.90601},
-        {"time": 9, "device": "green", "value": 53.87401},
-        {"time": 10, "device": "green", "value": 53.87401},
-    ]
-
+    data = get_device_data("green")
     labels = [label["time"] for label in data]
     green_values = [element["value"] for element in data]
     return render_template(
--- frontend/data.py
+++ frontend/data.py
@@ -1,10 +1,13 @@
 import redis
+import json
 
-_redis = redis.StrictRedis(
-    host="127.0.0.1",
-    port=6379,
-    db=1,
-)
+def get_device_data(device):
+    _redis = redis.StrictRedis(
+        host="127.0.0.1",
+        port=6379,
+        db=1,
+    )
 
-max_index = _redis.llen("green")
-data = _redis.lrange("green", 0, max_index)
+    max_index = _redis.llen(device)
+    data = _redis.lrange(device, 0, max_index)
+    return [json.loads(element) for element in data]
